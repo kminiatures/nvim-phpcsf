263c0a01c37be05563c18979784eb9a4e131866d
Hiroshi Kobayashi <h.kobayashi@sc-corp.co.jp>
2024-11-22 18:49:06 +0900

    Fix cs() for InsertLeave.

--- lua/phpcs/init.lua
+++ lua/phpcs/init.lua
@@ -16,46 +16,56 @@ M.last_stdout = ''
 M.nvim_namespace = nil
 
 M.detect_local_paths = function ()
-    if (lutils.file_exists('phpcs.xml')) then
-        M.phpcs_standard = root .. '/phpcs.xml'
-    end
+  if (lutils.file_exists('phpcs.xml')) then
+    M.phpcs_standard = root .. '/phpcs.xml'
+  end
 
-    if (lutils.file_exists('vendor/bin/phpcs')) then
-        M.phpcs_path = root .. '/vendor/bin/phpcs'
-    end
+  if (lutils.file_exists('vendor/bin/phpcs')) then
+    M.phpcs_path = root .. '/vendor/bin/phpcs'
+  end
 
-    if (lutils.file_exists('vendor/bin/phpcbf')) then
-        M.phpcbf_path = root .. '/vendor/bin/phpcbf'
-    end
+  if (lutils.file_exists('vendor/bin/phpcbf')) then
+    M.phpcbf_path = root .. '/vendor/bin/phpcbf'
+  end
 
-    M.nvim_namespace = vim.api.nvim_create_namespace("phpcs")
+  M.nvim_namespace = vim.api.nvim_create_namespace("phpcs")
 end
 
 M.cs = function ()
-	local bufnr = vim.api.nvim_get_current_buf()
+  local bufnr = vim.api.nvim_get_current_buf()
 
-    local report_file = os.tmpname();
 
-	local opts = {
-		command = M.phpcs_path,
-  		args = {
-    		"--stdin-path=" .. vim.api.nvim_buf_get_name(bufnr),
-			"--report=json",
-            "--report-file=" .. report_file,
-			"--standard=" .. M.phpcs_standard,
-    		"-"
-  		},
-      	writer = vim.api.nvim_buf_get_lines(bufnr, 0, -1, true),
-  		on_exit = vim.schedule_wrap(function()
-            local file = io.open(report_file, "r")
-            if file ~= nil then
-                local content = file:read("*a")
-                M.publish_diagnostic(content, bufnr)
-            end
-  		end),
-  	}
+  local buf_file = os.tmpname()..".php";
+  local buf_content = vim.api.nvim_buf_get_lines(bufnr, 0, -1, true);
+  local file = io.open(buf_file, "w")
+  for _, line in ipairs(buf_content) do
+    file:write(line .. "\n")
+  end
+  file:close()
 
-  	Job:new(opts):start()
+  local report_file = os.tmpname();
+
+  local opts = {
+    command = M.phpcs_path,
+    args = {
+      "--report=json",
+      "--report-file=" .. report_file,
+      "--standard=" .. M.phpcs_standard,
+      buf_file
+    },
+    writer = vim.api.nvim_buf_get_lines(bufnr, 0, -1, true),
+    on_exit = vim.schedule_wrap(function()
+      local file = io.open(report_file, "r")
+      if file ~= nil then
+        local content = file:read("*a")
+        M.publish_diagnostic(content, bufnr, buf_file)
+      end
+      os.remove(report_file)
+      os.remove(buf_file)
+    end),
+  }
+
+  Job:new(opts):start()
 end
 
 --[[
@@ -65,46 +75,47 @@ end
         timeout = 1000, -- Timeout in ms for the job. Default 1000ms
     }
 ]]
+
 M.cbf = function (new_opts)
-	new_opts = new_opts or {}
-  	new_opts.bufnr = new_opts.bufnr or vim.api.nvim_get_current_buf()
+  new_opts = new_opts or {}
+  new_opts.bufnr = new_opts.bufnr or vim.api.nvim_get_current_buf()
 
-  	if not new_opts.force then
-  		if vim.api.nvim_buf_line_count(new_opts.bufnr) > 1000 then
-  			print("File too large. Ignoring code beautifier" )
-  			return
-  		end
-  	end
+  if not new_opts.force then
+    if vim.api.nvim_buf_line_count(new_opts.bufnr) > 1000 then
+      print("File too large. Ignoring code beautifier" )
+      return
+    end
+  end
 
-	local opts = {
-		command = M.phpcbf_path,
-  		args = {
-			"--standard=" .. M.phpcs_standard,
-            vim.api.nvim_buf_get_name(new_opts.bufnr)
-  		},
-  		on_exit = vim.schedule_wrap(function(j)
-            if j.code ~= 0 then
-                vim.cmd("e")
-            end
-  		end),
-      	cwd = vim.fn.getcwd(),
-  	}
+  local opts = {
+    command = M.phpcbf_path,
+    args = {
+      "--standard=" .. M.phpcs_standard,
+      vim.api.nvim_buf_get_name(new_opts.bufnr)
+    },
+    on_exit = vim.schedule_wrap(function(j)
+      if j.code ~= 0 then
+        vim.cmd("e")
+      end
+    end),
+    cwd = vim.fn.getcwd(),
+  }
 
-  	Job:new(opts):start()
+  Job:new(opts):start()
 end
 
-M.publish_diagnostic = function (results, bufnr)
+M.publish_diagnostic = function (results, bufnr, buf_file)
 	bufnr = bufnr or vim.api.nvim_get_current_buf()
 
-    local diagnostics = parse_json(results, bufnr)
+    local diagnostics = parse_json(results, bufnr, buf_file)
 
     vim.diagnostic.set(M.nvim_namespace, bufnr, diagnostics)
 end
 
-function parse_json(encoded, bufnr)
+function parse_json(encoded, bufnr, buf_file)
     local decoded = vim.json.decode(encoded)
     local diagnostics = {}
-    local uri = vim.fn.bufname(bufnr);
+    local uri = buf_file
 
     local error_codes = {
         ['error'] = vim.lsp.protocol.DiagnosticSeverity.Error,
